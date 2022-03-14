d8dd50678c9455c5ca0696d3e9fcd429adfa03c1
Reem395 <reem.khalil95@yahoo.co.uk>
2022-03-14 23:56:34 +0200

    stablish app

---
+++ client/app-client/src/app/Employee/patient/patient-profile/patient-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-patient-profile',
+  templateUrl: './patient-profile.component.html',
+  styleUrls: ['./patient-profile.component.css']
+})
+export class PatientProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/app-client/src/app/Employee/patient/patient.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { PatientAddComponent } from './patient-add/patient-add.component';
+import { PatientEditComponent } from './patient-edit/patient-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PatientProfileComponent } from './patient-profile/patient-profile.component';
+
+
+const routes:Routes=[
+  {path:"",component:PatientListComponent},
+  {path:"add",component:PatientAddComponent},
+  {path:"edit/:id",component:PatientEditComponent},
+  {path:"profile/:id",component:PatientProfileComponent},
+]
+
+@NgModule({
+  declarations: [
+    PatientListComponent,
+  
+    // PatientAddComponent
+  
+    PatientEditComponent,
+       PatientProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    PatientListComponent,
+    PatientEditComponent
+  ]
+})
+export class PatientModule { }
