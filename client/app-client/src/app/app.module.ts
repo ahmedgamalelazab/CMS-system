f37a2fd8d10fe77ea0c842e129d70a6dd4b9e127
MoMazen <mostafamazen095@gmail.com>
2022-03-14 19:00:11 +0200

    pushing

--- client/app-client/src/app/app.module.ts
+++ client/app-client/src/app/app.module.ts
@@ -2,14 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeModule } from './Employee/employee.module';
 import * as $ from 'jquery';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = [
+  // {path:"home",component:HomeComponent},
+  
+  {path:"employee",loadChildren:()=>import("./Employee/employee.module").then(m=>m.EmployeeModule)},
+  {path:"",redirectTo:"/employee",pathMatch:"full"},
+  // {path:"students",loadChildren:()=>import("./students/students.module").then(m=>m.StudentsModule)},
+  // {path:"**",component:ErrorComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+    EmployeeModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
