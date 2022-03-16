bf8679505e64a97e558eef838ac6ce0c9bd62867
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-03-16 16:52:37 +0200

    added Manual dependencies

---
+++ client/app-client/src/app/apis/NotificationLib.ts
@@ -0,0 +1,34 @@
+/**
+ * @DATE : 16-3-2022
+ * @description : a Notification lib .. that our project client code can consume it
+ * @purposeOfDesign : support the states in the project
+ */
+
+//pubsub
+/**
+ * @description: sometimes we will need to subscribe under events in the project .. this api will provide all important features or client code will need
+ *
+ */
+class Pubsub{
+
+  //code ..
+
+}
+
+//subject observers
+/**
+ * @description: simpler than using angular [SUBJECT] pattern
+ */
+
+class Subject{
+
+  //code ..
+
+}
+
+//chain of responsibilities
+/**
+ * @description : it will manage the requests and notify our views with updates
+ */
+
+export {Pubsub , Subject}
