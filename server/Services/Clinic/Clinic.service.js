267295b455373667122453a36265ccc08bf2fbe0
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-03-21 06:17:47 +0200

    FIXED TRANSACTION BUGS

--- server/Services/Clinic/Clinic.service.js
+++ server/Services/Clinic/Clinic.service.js
@@ -38,51 +38,80 @@ async function AddClinicService(
   return new Promise(async (resolve, reject) => {
     if (DBConnection.isConnected()) {
       //if the DBConnection is ok
+      const session = await mongoose.startSession();
       try {
         const dataTracer = {};
         //transaction code
-        const session = await mongoose.startSession();
         await session.withTransaction(async () => {
-          const user = await User.create({
-            email: userEmail,
-            password: userPassword,
-            userType: 'doctor',
-          });
+          const user = await User.create(
+            [
+              {
+                email: userEmail,
+                password: userPassword,
+                userType: 'doctor',
+              },
+            ],
+            {
+              session: session,
+            }
+          );
           //doctor profile image will set automatically with default image but then we will allow change
           //to change the doctor profile image u will click on the doctor profile image then upload profile image then u will ask th server to upload to this guy profile image
-          const doctor = await Doctor.create({
-            name: docName,
-            age: docAge,
-            salary: docSalary,
-            user: user._id,
-            isOwner: iswOwner,
-            owner: docBoss,
-          });
-          const clinic = await Clinic.create({
-            name: clinicName,
-            address: clinicAddress,
-            phone: clinicPhone,
-            description: clinicDescription,
-          });
-          if (doctor.isOwner) {
-            clinic.owner = doctor._id;
+          const doctor = await Doctor.create(
+            [
+              {
+                name: docName,
+                age: docAge,
+                salary: docSalary,
+                user: user[0]._id,
+                isOwner: iswOwner,
+                owner: docBoss,
+              },
+            ],
+            {
+              session: session,
+            }
+          );
+          //test transactions : note : took me hours to understand this transaction system
+          //i've read almost all the documentation in order to get it 😂
+          // throw 'transaction test mongodb';
+          const clinic = await Clinic.create(
+            [
+              {
+                name: clinicName,
+                address: clinicAddress,
+                phone: clinicPhone,
+                description: clinicDescription,
+              },
+            ],
+            {
+              session: session,
+            }
+          );
+          if (doctor[0].isOwner) {
+            console.log('yes');
+            clinic[0].owner = doctor[0]._id;
           }
-          doctor.isConnectedToClinic = true;
-          clinic.doctors.push(doctor._id);
+          doctor[0].isConnectedToClinic = true;
+          console.log(clinic[0]);
+          console.log(clinic[0].doctors);
+          console.log(doctor[0]._id);
+          clinic[0].doctors.push(doctor[0]._id);
           ////////////////////////////////////////
           ////////////SAVING CODE ////////////////
           //? any code issue this transaction will not fll the data
-          await user.save();
-          await doctor.save();
-          await clinic.save();
+          await user[0].save();
+          await doctor[0].save();
+          await clinic[0].save();
           ////////////END OF SAVING CODE ////////////////
           ////////////////////////////////////////
           dataTracer.userData = user;
           dataTracer.doctorData = doctor;
           dataTracer.clinicData = clinic;
           //if all are ok
+          // throw 'test'; //transaction worked
         });
-        session.endSession(); //end transaction session
+        await session.endSession(); //end transaction session
         resolve({
           success: true,
           data: dataTracer,
@@ -153,13 +182,21 @@ async function eraseClinicData(clinicId) {
               },
               {
                 isConnectedToClinic: false,
+              },
+              {
+                session: session,
               }
             );
           });
 
-          const result = await Clinic.deleteOne({
-            _id: clinicId,
-          });
+          const result = await Clinic.deleteOne(
+            {
+              _id: clinicId,
+            },
+            {
+              session: session,
+            }
+          );
 
           //if it removed well
 
