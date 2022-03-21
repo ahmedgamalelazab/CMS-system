a0ddcac7464c07a24550ae7bd9ede109b6ce98b9
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-03-21 11:49:01 +0200

    crud service for patient added | transactions still working

---
+++ server/Services/patients/patientService.js
@@ -0,0 +1,148 @@
+const Patient = require('../../Models/Patient.js');
+const mongoose = require('mongoose'); //only for transaction purposes
+const { DBConnection } = require('../../DataBase/db.config.js');
+
+async function getAllPatientsService() {
+  return new Promise(async (resolve, reject) => {
+    if (DBConnection.isConnected()) {
+      try {
+        const patients = await Patient.find();
+        resolve({
+          success: true,
+          data: patients,
+        });
+      } catch (error) {
+        reject(new Error(error.message));
+      }
+    } else {
+      reject(new Error('db connection problem'));
+    }
+  });
+}
+
+/**
+ *
+ * @param {string} patientId
+ * @returns {Promise}
+ */
+async function getPatientByIdService(patientId) {
+  return new Promise(async (resolve, reject) => {
+    if (DBConnection.isConnected()) {
+      try {
+        const patient = await Patient.findOne({
+          _id: patientId,
+        });
+        resolve({
+          success: true,
+          data: patient,
+        });
+      } catch (error) {
+        reject(new Error(error.message));
+      }
+    } else {
+      reject(new Error('db connection problem'));
+    }
+  });
+}
+
+/**
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {number} age
+ * @param {number} gender
+ * @param {string} phone
+ * @param {string} clinicId
+ * @returns  {Promise}
+ */
+async function addPatientService(
+  firstName,
+  lastName,
+  age,
+  gender,
+  phone,
+  clinicId //this id later on will be added through out the employee search clinic data
+) {
+  return new Promise(async (resolve, reject) => {
+    if (DBConnection.isConnected()) {
+      try {
+        const patient = await Patient.create({
+          firstName: firstName,
+          lastName: lastName,
+          age: age,
+          gender: gender,
+          phone: phone,
+          clinicId: clinicId,
+        });
+
+        //if all are ok
+
+        resolve({
+          success: true,
+          data: patient,
+        });
+      } catch (error) {
+        reject(new Error(error.message));
+      }
+    } else {
+      reject(new Error('db connection problem'));
+    }
+  });
+}
+
+async function updatePatientService(
+  patientId,
+  firstName,
+  lastName,
+  age,
+  gender,
+  phone
+) {
+  return new Promise(async (resolve, reject) => {
+    if (DBConnection.isConnected()) {
+      const dataTracer = {};
+      try {
+        //the employee not allowed to edit his clinic id or any one else allowed
+        const updatePatient = await Patient.updateOne(
+          {
+            _id: patientId,
+          },
+          {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            gender: gender,
+            phone: phone,
+          }
+        );
+        const updatedPatient = await Patient.findOne({
+          _id: patientId,
+        });
+
+        //if all are ok
+        dataTracer.updatedPatientData = updatedPatient;
+        dataTracer.updatedRecord = true;
+        resolve({
+          success: true,
+          data: dataTracer,
+        });
+      } catch (error) {
+        reject(new Error(error.message));
+      }
+    } else {
+      reject(new Error('db connection error'));
+    }
+  });
+}
+
+/**
+ *
+ * @param {string} patientId
+ * @returns {Promise}
+ */
+async function deletePatientService(patientId) {
+  return new Promise(async (resolve, reject) => {
+    if (DBConnection.isConnected()) {
+    } else {
+    }
+  });
+}
