56d5fba12f1c7539b009a0bf58eda3d5923c2bde
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-03-21 12:16:04 +0200

    patients crud complete |transactions working

--- server/Controllers/patient/patient.controller.js
+++ server/Controllers/patient/patient.controller.js
@@ -1,4 +1,11 @@
 const { request, response } = require('express');
+const {
+  addPatientService,
+  deletePatientService,
+  getAllPatientsService,
+  getPatientByIdService,
+  updatePatientService,
+} = require('../../Services/patients/patientService.js');
 
 /**
  *
@@ -7,22 +14,41 @@ const { request, response } = require('express');
  * @param {Function} next
  * @description this function is only allowed for the system admin and not allowed to doctors
  */
-module.exports.getAllPatientsController = async (req, res, next) => {};
-/**
- *
- * @param {request} req
- * @param {response} res
- * @param {Function} next
- */
-module.exports.doctorGetAllPatientsController = async (req, res, next) => {};
-/**
- *
- * @param {request} req
- * @param {response} res
- * @param {Function} next
- * @description this function is only allowed for the system admin and not allowed to doctors
- */
-module.exports.doctorAddPatientController = async (req, res, next) => {};
+module.exports.getAllPatientsController = async (req, res, next) => {
+  try {
+    const result = await getAllPatientsService();
+
+    //if all are ok
+
+    res.status(200).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(501).json({
+      success: false,
+      data: null,
+      errorMessage: error.message,
+    });
+  }
+};
+
+module.exports.getPatientByIdController = async (req, res, next) => {
+  try {
+    const patientId = req.params.id;
+    const result = await getPatientByIdService(patientId);
+    //if all are ok
+    res.status(200).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(501).json({
+      success: false,
+      data: null,
+    });
+  }
+};
 
 /**
  *
@@ -31,7 +57,33 @@ module.exports.doctorAddPatientController = async (req, res, next) => {};
  * @param {Function} next
  * @description this function is only allowed for the system admin and not allowed to doctors
  */
-module.exports.doctorUpdatePatientController = async (req, res, next) => {};
+module.exports.doctorAddPatientController = async (req, res, next) => {
+  try {
+    const { firstName, lastName, age, gender, phone, clinicId } = req.body;
+
+    const result = await addPatientService(
+      firstName,
+      lastName,
+      age,
+      gender,
+      phone,
+      clinicId
+    );
+
+    //if all are ok
+
+    res.status(201).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(501).json({
+      success: false,
+      data: null,
+      errorMessage: error.message,
+    });
+  }
+};
 
 /**
  *
@@ -40,4 +92,58 @@ module.exports.doctorUpdatePatientController = async (req, res, next) => {};
  * @param {Function} next
  * @description this function is only allowed for the system admin and not allowed to doctors
  */
-module.exports.doctorRemovePatientController = async (req, res, next) => {};
+module.exports.doctorUpdatePatientController = async (req, res, next) => {
+  try {
+    const patientId = req.params.id;
+
+    const { firstName, lastName, age, gender, phone } = req.body;
+
+    const result = await updatePatientService(
+      patientId,
+      firstName,
+      lastName,
+      age,
+      gender,
+      phone
+    );
+    //if all are ok
+    res.status(201).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(501).json({
+      success: false,
+      data: null,
+      errorMessage: error.message,
+    });
+  }
+};
+
+/**
+ *
+ * @param {request} req
+ * @param {response} res
+ * @param {Function} next
+ * @description this function is only allowed for the system admin and not allowed to doctors
+ */
+module.exports.doctorRemovePatientController = async (req, res, next) => {
+  try {
+    const patientId = req.params.id;
+
+    const result = await deletePatientService(patientId);
+
+    //if all are ok
+
+    res.status(201).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(501).json({
+      success: false,
+      data: null,
+      errorMessage: error.message,
+    });
+  }
+};
