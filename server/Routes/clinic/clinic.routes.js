d874b1afa9c0bab4edcbdb3fa20faec8660c21af
ahmedgamalelazab <49871250+ahmedgamalelazab@users.noreply.github.com>
2022-03-23 02:37:34 +0200

    protected admin clinic routes

--- server/Routes/clinic/clinic.routes.js
+++ server/Routes/clinic/clinic.routes.js
@@ -9,13 +9,21 @@ const {
   ownerAssignClinicDoctor,
 } = require('../../Controllers/clinic/clinic.controller.js');
 
-//TODO only who will be able to execute those in system admin only
-//TODO implement the Auth logic middleware and inject it to the routes
+const { authMiddleWare } = require('../../Middlewares/auth.js');
 
-Router.route('/clinics').get(getAllClinicsController);
-Router.route('/clinics/:id/doctors').get(getAllClinicsDoctorsController);
-Router.route('/clinics/add').post(addClinicController);
-Router.route('/clinics/update/:id').put(updateClinicDataController);
-Router.route('/clinics/delete/:id').delete(removeClinicDataController);
+Router.route('/clinics').get(authMiddleWare, getAllClinicsController);
+Router.route('/clinics/:id/doctors').get(
+  authMiddleWare,
+  getAllClinicsDoctorsController
+);
+Router.route('/clinics/add').post(authMiddleWare, addClinicController);
+Router.route('/clinics/update/:id').put(
+  authMiddleWare,
+  updateClinicDataController
+);
+Router.route('/clinics/delete/:id').delete(
+  authMiddleWare,
+  removeClinicDataController
+);
 
 module.exports = Router;
